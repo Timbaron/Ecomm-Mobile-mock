d2a6c470af4848ebc6f889640679ceab4b1e0bbd
timbaron <timothy.akiode@student.aul.edu.ng>
2023-04-14 11:00:59 +0100

    Project initialization with file structure

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import Home from './Home';
+import Banner from './banner';
+import categories from './categories';
+import Footer from './footer';
+import Header from './Header';
+import Products from './Products';
+import Item from './item';
+
+export {Home,Banner,categories,Footer,Header,Products,Item}
