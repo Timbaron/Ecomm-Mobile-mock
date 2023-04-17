56cf6e9138b9e9abb1fcd8253ee79057f9bf29cc
timbaron <timothy.akiode@student.aul.edu.ng>
2023-04-17 10:08:15 +0100

    work header inintial design

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,14 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {Header, Banner, Categories, Products} from './'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Header />
+        <Banner />
+        <Categories />
+        <Products />
+      
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      padding: 24,
+    },
+    main: {
+      flex: 1,
+      justifyContent: "center",
+      maxWidth: 960,
+      marginHorizontal: "auto",
+    },
+    title: {
+      fontSize: 64,
+      fontWeight: "bold",
+    },
+    subtitle: {
+      fontSize: 36,
+      color: "#38434D",
+    },
+  });
